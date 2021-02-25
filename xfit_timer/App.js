e7bfbb687858ba750c82524d1fdf68c225a3d4a9
stevekutz <stkutz@gmail.com>
2021-02-24 20:56:02 -0600

    added initial Timer comp

--- xfit_timer/App.js
+++ xfit_timer/App.js
@@ -2,10 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Timer from './comp/Timer';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text> default </Text>
+
+      <Timer/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ xfit_timer/comp/Timer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+
+const Timer = (props) => {
+    return (
+        <View>
+            <Text> Timer Comp </Text>
+        </View>
+    )
+
+}
+
+export default Timer;
